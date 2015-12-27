a30d0b774959c8a0d8ab76d704b7da91db39610e
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-12-27 13:14:37 +0530

    added follow parts as well :punch:

---
+++ src/ISP/follow/BSEBroker.java
@@ -0,0 +1,34 @@
+package ISP.follow;
+
+import java.util.Date;
+
+public class BSEBroker implements BrokerStatistics,BrokerTransactions{
+    
+    double er=0.0,lo=0.0;
+    
+    @Override
+    public void buy(String symbol, double vol) {
+        //code to buy stocks
+    }
+
+    @Override
+    public void sell(String symbol, double vol) {
+        //code to sell stocks
+    }
+
+    @Override
+    public double dailyLoss(Date date) {
+        //compute daily loss
+        return lo;
+    }
+
+    @Override
+    public double dailyEarnings(Date date) {
+        //compute daily earnings
+        return er;
+    }
+    
+    public void currentBalance(){
+        //computes current balance
+    }
+}
